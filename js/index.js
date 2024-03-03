d24ede9e1d726cade4753fec8c3ada6cf0173ae5
Abdullah3d <abdullah.ibrarhim.609@gmail.com>
2024-03-03 21:19:36 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,28 @@
 const loadCard = async() => {
-    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
-    const card = data;
-    displayCards(card);
+    const cards = data;
+    displayCards(cards);
 }
-const displayCards = card => {
-    console.log(card)
-}
-card.forEach (card => {
-    console.log(card);
-    
-});
+const displayCards = cards => {
+    // console.log(card)
+   const cardContainer1 = document.getElementById('card-container');
 
-// loadCard();
+    cards.posts.forEach ((card) => {
+        console.log(card);
+
+         const cardContainer = document.createElement('div');
+         cardContainer.classList = `card w-96 bg-gray-100 shadow-xl`;
+         cardContainer.innerHTML = `
+         <div class="card-body">
+         <h2 class="card-title"></h2>
+         <p>If a dog chews shoes whose shoes does he choose?</p>
+         <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+         </div>
+         `;
+         cardContainer1.appendChild(cardContainer);
+    });
+}
+
+loadCard();
